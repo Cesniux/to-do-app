bec45e7788eefe411c6d5f843cfa8992c81940b5
Cesniux <74120990+Cesniux@users.noreply.github.com>
2021-02-15 22:12:52 +0200

    Redoing - js in action 2

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const textInput = document.getElementById('textInput');
+const addButton = document.getElementById('addButton');
+const list = document.getElementById('list')
+
+const LIST_ITEMS = [];
+
+addButton.addEventListener('click', () => {
+  alert(textInput.value);
+  LIST_ITEMS.push(textInput.value);
+  console.log(LIST_ITEMS);
+});
+
+const listItem = () => {}
+const renderList = () => {
+  list.innerHTML = '...';
+}
+
